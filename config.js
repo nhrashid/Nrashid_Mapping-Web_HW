bc83f277111340f2c60356af91c12ad0987f2c29
nhrashid <43556537+nhrashid@users.noreply.github.com>
2019-02-25 04:15:05 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
+//const API_KEY = "pk.eyJ1IjoibnJhc2hpZCIsImEiOiJjanNrNGF1c3cyNDdkNDlxZ3J0ZWhsdGd1In0.1YC1svNEkza8PJJftkFhNQ";
